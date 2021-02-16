50cd7d843be56d000f17c0f565f09611e3044d10
nofprog <k.jin3217@gmail.com>
2021-02-16 19:27:24 +0900

    Closure

---
+++ 100DaysOfSwift.playground/Pages/Closure.xcplaygroundpage/Contents.swift
@@ -0,0 +1,100 @@
+import UIKit
+
+let driving = {
+    print("drive my car")
+}
+driving()
+
+print("----------------")
+
+
+//戻り値を設定したらreturnを記述
+let driving_2 = {(place: String) -> String in
+    return ("I'go to \(place) in my car")
+}
+
+let message = driving_2("Mt.Fuji")
+print("message:", message)
+
+print("----------------")
+
+
+func travel(action: () -> Void){
+    print("ready to go")
+    action()
+    print("arrived")
+}
+//travel(action: driving)
+
+//trailing closure syntax
+travel() {
+    print("driving in my car")
+}
+//↑()は省略可能
+
+print("----------------")
+
+func travel_2(action: (String) -> Void ){
+    print("ready to go")
+    action("DisneyLand")
+    print("arrived")
+}
+
+travel_2 {(place: String) in
+    print("go to \(place) in my car")
+}
+/*
+(実行結果)
+ready to go
+go to Disneyland in my car
+arrived
+ */
+
+print("----------------")
+
+func travel_3(action: (String) -> String){
+    print("ready to go")
+    let description = action("DisneySea")
+    print(description)
+    print("arrived")
+}
+
+/*
+travel_3 {(place:String) -> String in
+    return "go to \(place) in my car"
+}
+ ↓省略形
+*/
+
+travel_3 { place in
+    "go to \(place) in my car"
+}
+
+print("----------------")
+
+
+//複数のパラメータver
+func travel_4(action: (String, Int) -> String) {
+    print("ready to go")
+    let description  =  action("USJ", 60)
+    print(description)
+    print("arrived")
+}
+
+travel_4 {
+    "I'm goiing to \($0) at \($1) miiles per hour"
+}
+//$0 → 一個目、$1 → 二個目
+
+print("----------------")
+
+func travel_5() ->  (String) -> Void {
+    return {
+        print("I'm going to \($0)")
+    }
+}
+
+let result = travel_5()
+result("Tokyo")
+
+let result_2 = travel_5()("Hokkaido")
